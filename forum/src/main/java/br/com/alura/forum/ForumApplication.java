1bed97140dd5b3db17891ef859205153dcb35e0e
Erick-Fernandes-dev <erick.farias@dcx.ufpb.br>
2022-09-07 09:04:40 -0300

    Project 2 : API Rest with Spring Boot

--- forum/src/main/java/br/com/alura/forum/ForumApplication.java
+++ forum/src/main/java/br/com/alura/forum/ForumApplication.java
@@ -2,11 +2,13 @@ package br.com.alura.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport//Habilita o suporte para que o Spring pegue da requisição dos parâmetros da URL,
 // Os campos, as informações de paginação e ordenação, e repassar isso para o Spring data.
+@EnableCaching //Habilitando o cache
 public class ForumApplication {
 
 	public static void main(String[] args) {
